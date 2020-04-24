f855ad9c8b8ec5a80e68bae069f5fca6c1530160
Michael Yu <1619025+acrylix@users.noreply.github.com>
2020-04-24 18:17:29 -0400

    builder + ethers + waffle + typechain

---
+++ utils/configLoader.ts
@@ -0,0 +1,39 @@
+import * as dotenv from 'dotenv'
+import * as path from 'path'
+import { HttpNetworkConfig } from '@nomiclabs/buidler/types'
+
+type EthereumNetworksTypes =
+  | 'mainnet'
+  | 'rinkeby'
+  | 'ropsten'
+  | 'kovan'
+  | 'goerli'
+
+type EnvironmentTypes = 'dev' | 'stg' | 'prod'
+
+export const networkConfig = (
+  network: EthereumNetworksTypes,
+  env: EnvironmentTypes
+): HttpNetworkConfig => {
+  const envFile = path.resolve(__dirname, `../config/${env}.env`)
+  const envLoad = dotenv.config({ path: envFile })
+
+  if (envLoad.error) {
+    throw new Error(envLoad.error.message)
+  }
+
+  const config = envLoad.parsed
+
+  if (!config) {
+    throw new Error(`unable to load config: ${env}`)
+  }
+
+  const networkConfig: HttpNetworkConfig = {
+    url: `https://${network}.infura.io/v3/${config['INFURA_API_KEY']}`,
+    accounts: {
+      mnemonic: config['ETH_MNEMONIC']
+    }
+  }
+
+  return networkConfig
+}
